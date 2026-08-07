2bcdc8253776a73d06de3ed5a81fe12875b352e2
فلاح <mf751f@gmail.com>
2026-08-08 00:33:55 +0300

    Daemon: error.go remove printing

--- internal/daemon/errors.go
+++ internal/daemon/errors.go
@@ -1,25 +1,11 @@
 package daemon
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/mf751/btenl.git/internal/logger"
 	"github.com/mf751/btenl.git/internal/types"
 )
-
-const (
-	RESET   string = "\033[0m"
-	RED     string = "\033[31m"
-	GREEN   string = "\033[32m"
-	BLUE    string = "\033[34m"
-	MAGENTA string = "\033[35m"
-	CYAN    string = "\033[36m"
-)
-
-func prettierDate(t time.Time) string {
-	return t.Format("2006/01/02 15:04:05")
-}
 
 func eventTypeString(t types.EventType) string {
 	switch t {
@@ -28,17 +14,6 @@ func eventTypeString(t types.EventType) string {
 	default:
 		return "EVENT ERROR"
 	}
-}
-
-func printErrorEvent(event types.ErrorEvent) {
-	fmt.Print("[" + RED + "EVENTERROR" + RESET + "]")
-	fmt.Print("[" + BLUE + "DATE" + RESET + "]")
-	fmt.Print("[" + RED + prettierDate(event.StartedAt) + RESET + "]")
-	fmt.Print("[" + BLUE + "TYPE" + RESET + "]")
-	fmt.Print("[" + RED + eventTypeString(event.Type) + RESET + "]")
-	fmt.Print("[" + BLUE + "MESSAGE" + RESET + "]")
-	fmt.Print("[" + RED + event.Err.Error() + RESET + "]")
-	fmt.Println()
 }
 
 func logErrorEvent(logger *logger.Logger, event types.ErrorEvent) {
@@ -51,6 +26,5 @@ func logErrorEvent(logger *logger.Logger, event types.ErrorEvent) {
 }
 
 func (d *Daemon) handleErrorEvent(event types.ErrorEvent) {
-	printErrorEvent(event)
 	logErrorEvent(d.logger, event)
 }
