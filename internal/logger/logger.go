77332f43409c2baf2be73d133459a23d7646fdb1
فلاح <mf751f@gmail.com>
2026-08-07 23:57:01 +0300

    Simple logging package

---
+++ internal/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"time"
+)
+
+type Level string
+
+const (
+	INFO  Level = "INFO"
+	WARN  Level = "WARN"
+	ERROR Level = "ERROR"
+)
+
+type Logger struct {
+	logger *log.Logger
+}
+
+func New(out io.Writer) *Logger {
+	return &Logger{
+		logger: log.New(out, "", 0),
+	}
+}
+
+func (l *Logger) Log(level Level, msg string) {
+	l.logger.Printf("%s %s %s", time.Now().Format(time.RFC3339), level, msg)
+}
+
+func (l *Logger) Info(msg string) {
+	l.Log(INFO, msg)
+}
+
+func (l *Logger) Warn(msg string) {
+	l.Log(WARN, msg)
+}
+
+func (l *Logger) Error(err error) {
+	l.Log(ERROR, err.Error())
+}
+
+func (l *Logger) Errorf(format string, args ...any) {
+	l.Log(ERROR, fmt.Sprintf(format, args...))
+}
